1b7c245aeefb9a36a603ac7c75e9810fa52b72dc
Gastón Frenkel <gst.frenkel@gmail.com>
2024-09-02 16:49:17 -0300

    fix test

--- generar-compose.sh
+++ generar-compose.sh
@@ -24,7 +24,6 @@ generate_compose_file() {
     echo "      - ./server/config.ini:/config.ini" >> $COMPOSE_FILE
     echo "    environment:" >> $COMPOSE_FILE
     echo "      - PYTHONUNBUFFERED=1" >> $COMPOSE_FILE
-    echo "      - LOGGING_LEVEL=DEBUG" >> $COMPOSE_FILE
     echo "    networks:" >> $COMPOSE_FILE
     echo "      - testing_net" >> $COMPOSE_FILE
     echo "" >> $COMPOSE_FILE
